7de583ed9a2bb0e9c09f63a5216a902f5737ba07
Anusha Vernekar <128282051+AnushaSv@users.noreply.github.com>
2024-05-07 17:02:52 +0530

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n(+j@b&_uquwqyki2kzuf6ft3+%^u=ubo^@_o2p+yc-fc5sm_o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
